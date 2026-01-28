2b88714dd578868c2843fc348f71629d99546be5
TDOMxHUNTER <fazalur1004@gmail.com>
2026-01-28 23:29:40 +0530

    Auto-generate missing bootloader configs in docker build

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -9,8 +9,22 @@ docker build -t hunter-builder .
 echo ">>> Starting Build Process..."
 # We mount the current directory to /hunter-os inside the container
 # We also need privileged mode to mount loop devices for the ISO
-docker run --privileged --rm -v "$(pwd):/hunter-os" hunter-builder \
+# We use 'bash -c' to copy missing configs (grub/syslinux) from the default profile if they don't exist
+docker run --privileged --rm -v "$(pwd):/hunter-os" hunter-builder /bin/bash -c "
+    if [ ! -d /hunter-os/grub ]; then
+        echo '>>> Copying default GRUB config...'
+        cp -r /usr/share/archiso/configs/releng/grub /hunter-os/
+    fi
+    if [ ! -d /hunter-os/syslinux ]; then
+        echo '>>> Copying default Syslinux config...'
+        cp -r /usr/share/archiso/configs/releng/syslinux /hunter-os/
+    fi
+    if [ ! -d /hunter-os/efiboot ]; then
+        echo '>>> Copying default Efiboot config...'
+        cp -r /usr/share/archiso/configs/releng/efiboot /hunter-os/
+    fi
     mkarchiso -v -w /tmp/archiso-work -o /hunter-os/out .
+"
 
 echo ">>> Build Complete!"
 echo ">>> ISO should be in the 'out' directory."
